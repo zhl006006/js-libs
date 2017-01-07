101b6e5a6a2a260caac8ce5d5d293965fffe0cfb
tb <35296097@qq.com>
2017-01-07 19:46:45 +0800

    增加time功能

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ Href封装了超链接功能，支持get和post，参数使用对象
 <br/>
 Href.get(url,data);<br/>
 Href.post(url,data);<br/>
+<br/>
+<br/>
+文件名time.js:<br/>
+Time封装了时间处理相关功能
+<br/>
+<br/>
+同步服务器时间<br/>
+Time.syncServer(url,callback);<br/>
+获取服务器时间，单位：秒 (只有在syncServer回调完成之后才有效)<br/>
+Time.getServerTime();<br/>
+UTC时间转本地时间：参数和返回单位：秒 <br/>
+Time.toLocal(utc);<br/>
---
+++ src/time.js
@@ -0,0 +1,41 @@
+/**
+ * Created by tb on 2017/1/7.
+ */
+var Time = (function () {
+    var _syncTime = 0;
+    var _serverTime = 0;
+    var o = {};
+    o.syncServer = function(url,callback)
+    {
+        var xhr = null;
+        if(window.XMLHttpRequest){
+            xhr = new window.XMLHttpRequest();
+        }else{ // ie
+            xhr = new ActiveObject("Microsoft")
+        }
+        // 通过get的方式请求当前文件
+        xhr.open("get",url);
+        xhr.send(null);
+        // 监听请求状态变化
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState===2){
+                _syncTime = new Date().getTime();
+                // 获取响应头里的时间戳
+                var time = xhr.getResponseHeader("Date");
+                _serverTime = new Date(time).getTime();
+                if (callback)
+                {
+                    callback();
+                }
+            }
+        }
+    }
+    o.getServerTime = function () {
+        return Math.floor((new Date().getTime() - _syncTime + _serverTime)/1000);
+    }
+    o.toLocal = function(utc)
+    {
+        return utc - new Date().getTimezoneOffset()*60;
+    }
+    return o;
+})();
