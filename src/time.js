654faa947177e4e68240a3c5c6555badbb191834
tb <35296097@qq.com>
2017-01-07 19:53:32 +0800

    改进

--- src/time.js
+++ src/time.js
@@ -1,24 +1,30 @@
 /**
  * Created by tb on 2017/1/7.
  */
-var Time = (function () {
+var Time = (function ()
+{
     var _syncTime = 0;
     var _serverTime = 0;
     var o = {};
     o.syncServer = function(url,callback)
     {
         var xhr = null;
-        if(window.XMLHttpRequest){
+        if(window.XMLHttpRequest)
+        {
             xhr = new window.XMLHttpRequest();
-        }else{ // ie
+        }
+        else
+        { // ie
             xhr = new ActiveObject("Microsoft")
         }
         // 通过get的方式请求当前文件
         xhr.open("get",url);
         xhr.send(null);
         // 监听请求状态变化
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState===2){
+        xhr.onreadystatechange = function()
+        {
+            if(xhr.readyState===2)
+            {
                 _syncTime = new Date().getTime();
                 // 获取响应头里的时间戳
                 var time = xhr.getResponseHeader("Date");
@@ -30,7 +36,8 @@ var Time = (function () {
             }
         }
     }
-    o.getServerTime = function () {
+    o.getServerTime = function ()
+    {
         return Math.floor((new Date().getTime() - _syncTime + _serverTime)/1000);
     }
     o.toLocal = function(utc)
