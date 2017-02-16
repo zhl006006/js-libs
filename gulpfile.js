1782c1ddd87cc57386b79aaa97360258ee29d7a7
tb <35296097@qq.com>
2017-02-16 16:12:28 +0800

    使用gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+gulp.task('default',function()
+{
+    //js 合并 压缩
+    var uglify=require('gulp-uglify');
+    var concat = require('gulp-concat');
+    gulp.src('src/*.js').pipe(uglify()).pipe(gulp.dest('dist'));
+    //gulp.src('src/*.js').pipe(concat('bundle.min.js')).pipe(uglify()).pipe(gulp.dest('dist'));
+});
