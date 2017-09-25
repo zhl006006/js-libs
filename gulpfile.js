07d03e39705e155291f73581bd3ea49704cfbe7e
tb <35296097@qq.com>
2017-09-25 11:08:37 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-var gulp = require('gulp');
-gulp.task('default',function()
+const Gulp = require('gulp');
+const GulpUtil = require('./plugins/gulp-util');
+Gulp.task('default',function()
 {
+    const DistPath = './dist';
+    //
+    GulpUtil.deleteDirectory(DistPath);
     //js 合并 压缩
-    var uglify=require('gulp-uglify');
-    var concat = require('gulp-concat');
-    gulp.src('src/*.js').pipe(uglify()).pipe(gulp.dest('dist'));
-    //gulp.src('src/*.js').pipe(concat('utils.min.js')).pipe(uglify()).pipe(gulp.dest('dist'));
+    const Uglify=require('gulp-uglify');
+    const Concat = require('gulp-concat');
+    const Rename = require("gulp-rename");
+    Gulp.src('src/*.js').pipe(Uglify()).pipe(Rename({suffix:".min"})).pipe(Gulp.dest(DistPath));
+    Gulp.src('src/*.js').pipe(Concat('utils.min.js')).pipe(Uglify()).pipe(Gulp.dest(DistPath));
 });
